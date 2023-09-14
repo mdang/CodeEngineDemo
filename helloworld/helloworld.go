598d2c98a31f3a55e72a2cc86e4f562e4395d56d
Mike Dang <dang@ibm.com>
2023-09-14 10:43:43 -0500

    Testing

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -57,7 +57,6 @@ func Debug(doit bool, format string, args ...interface{}) {
 // Just print an cool essage to the Writer that's passed in
 func PrintMessage(w io.Writer, showAll bool) {
 	fmt.Fprintf(w, "%s:\n", msg)
-	fmt.Fprintln(w, `IBM Cloud Cafe`)
 	fmt.Fprintln(w, `. ___  __  ____  ____`)
 	fmt.Fprintln(w, `./ __)/  \(    \(  __)`)
 	fmt.Fprintln(w, `( (__(  O )) D ( ) _)`)
@@ -66,6 +65,7 @@ func PrintMessage(w io.Writer, showAll bool) {
 	fmt.Fprintln(w, `(  __)(  ( \ / __)(  )(  ( \(  __)`)
 	fmt.Fprintln(w, `.) _) /    /( (_ \ )( /    / ) _)`)
 	fmt.Fprintln(w, `(____)\_)__) \___/(__)\_)__)(____)`)
+	fmt.Fprintln(w, `..........................`)
 	fmt.Fprintln(w, "")
 
 	fmt.Fprintf(w, "Some Env Vars:\n")
